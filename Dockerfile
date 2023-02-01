1a700ff5bf06b3997e32a72ad664e1b580d11e37
Alex Hayton <alex.hayton@gmail.com>
2023-02-01 16:55:32 +0000

    Initial build

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM cimg/rust:1.66.1
+
+COPY ./install-dependencies.sh .
+RUN sudo ./install-dependencies.sh
+
+RUN cargo install cargo-c@0.9.15+cargo-0.67 
+RUN cargo install cargo-watch
---
+++ install-dependencies.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+export CLANG_VERSION=12
+
+# This list is huge. Need to slim it down.
+sed -i 's/# deb-src/deb-src/g' /etc/apt/sources.list
+apt-get update
+apt-get dist-upgrade -y
+apt-get install -y --no-install-recommends \
+  ca-certificates \
+  git \
+  wget \
+  python3 \
+  python3-pip \
+  gcc \
+  clang-$CLANG_VERSION \
+  cmake \
+  libatk1.0-dev \
+  libatk-bridge2.0-dev \
+  libatspi2.0-dev \
+  libcups2-dev \
+  libxcomposite-dev \
+  libxdamage-dev \
+  libunwind-dev \
+  libdw-dev \
+  libgmp-dev \
+  libgraphene-1.0-dev \
+  libgsl-dev \
+  libglib2.0-dev \
+  flex \
+  bison \
+  libcap-dev \
+  libgirepository1.0-dev \
+  gettext \
+  liborc-0.4-dev \
+  iso-codes \
+  libgl-dev \
+  libgles-dev \
+  libdrm-dev \
+  libgudev-1.0-dev \
+  libgbm-dev \
+  libpng-dev \
+  libjpeg-dev \
+  libogg-dev \
+  libopus-dev \
+  libpango1.0-dev \
+  libvisual-0.4-dev \
+  libtheora-dev \
+  libvorbis-dev \
+  libxkbcommon-dev \
+  libwayland-dev \
+  libepoxy-dev \
+  ruby \
+  libgcrypt20-dev \
+  libwebp-dev \
+  libopenjp2-7-dev \
+  libwoff-dev \
+  libxslt1-dev \
+  bubblewrap \
+  libseccomp-dev \
+  xdg-dbus-proxy \
+  gperf \
+  libsoup2.4-dev \
+  libvulkan-dev \
+  libass-dev \
+  libchromaprint-dev \
+  libcurl4-gnutls-dev \
+  libaom-dev \
+  libbz2-dev \
+  liblcms2-dev \
+  libbs2b-dev \
+  libdca-dev \
+  libfaac-dev \
+  libfaad-dev \
+  flite1-dev \
+  libssl-dev \
+  libfdk-aac-dev \
+  libfluidsynth-dev \
+  libgsm1-dev \
+  libkate-dev \
+  libgme-dev \
+  libde265-dev \
+  liblilv-dev \
+  libmodplug-dev \
+  libmjpegtools-dev \
+  libmpcdec-dev \
+  libdvdnav-dev \
+  libdvdread-dev \
+  librsvg2-dev \
+  librtmp-dev \
+  libsbc-dev \
+  libsndfile1-dev \
+  libsoundtouch-dev \
+  libspandsp-dev \
+  libsrt-gnutls-dev \
+  libsrtp2-dev \
+  libvo-aacenc-dev \
+  libvo-amrwbenc-dev \
+  libwebrtc-audio-processing-dev \
+  libofa0-dev \
+  libzvbi-dev \
+  libopenexr-dev \
+  libwildmidi-dev \
+  libx265-dev \
+  libzbar-dev \
+  wayland-protocols \
+  libaa1-dev \
+  libmp3lame-dev \
+  libcaca-dev \
+  libdv4-dev \
+  libmpg123-dev \
+  libvpx-dev \
+  libshout3-dev \
+  libspeex-dev \
+  libtag1-dev \
+  libtwolame-dev \
+  libwavpack-dev \
+  liba52-0.7.4-dev \
+  libx264-dev \
+  libopencore-amrnb-dev \
+  libopencore-amrwb-dev \
+  libmpeg2-4-dev \
+  libavcodec-dev \
+  libavfilter-dev \
+  libavformat-dev \
+  libavutil-dev \
+  libva-dev \
+  libudev-dev \
+  glibc-tools \
+  libsoup-3.0-dev \
+  libwayland-client0 \
+  libwayland-egl1 \
+  libwayland-server0 \
+  libepoxy0 \
+  libwoff1 \
+  libwebp7 \
+  libegl1-mesa-dev \
+  libcairo2 \
+  libfontconfig1 \
+  libfreetype6 \
+  g++ \
+  libcairo2-dev \
+  libgnutls28-dev \
+  libharfbuzz-dev \
+  libicu-dev \
+  libxml2-dev \
+  pkg-config \
+  zlib1g-dev \
+  graphviz \
+  xvfb \
+  x11-utils \
+  xserver-xorg-core \
+  xserver-xorg-dev \
+  xorg \
+  libgstreamer1.0-dev \
+  libgstreamer-plugins-base1.0-dev \
+  libgstreamer-plugins-bad1.0-dev \
+  gstreamer1.0-plugins-base \
+  gstreamer1.0-plugins-good \
+  gstreamer1.0-plugins-bad \
+  gstreamer1.0-plugins-ugly \
+  gstreamer1.0-libav \
+  gstreamer1.0-tools \
+  gstreamer1.0-alsa \
+  gstreamer1.0-gl \
+  gstreamer1.0-pulseaudio \
+  ninja-build \
+  meson
+
+# ARCH=$(dpkg --print-architecture)
+# if [[ $ARCH == "amd64" ]]; then
+#    apt-get install -y --no-install-recommends \
+#      libmfx-dev \
+#      intel-media-va-driver-non-free
+# fi
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
